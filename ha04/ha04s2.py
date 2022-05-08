eeae90590c1a8a777d61561a327d6980722d5a4f
Evgenii Bykov <bikovevg@gmail.com>
2022-05-08 14:29:20 +0300

    added solid principles

--- ha04/ha04s2.py
+++ ha04/ha04s2.py
@@ -1,11 +1,57 @@
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+class Liquid(Product):
+    pass
+
+
+class Snack(Product):
+    pass
+
+
+class ProductFactory:
+    @staticmethod
+    def create_product(name):
+        price_dict = {
+            'CocaCola': 135,
+            'Sprite': 120,
+            'Alenka': 60,
+            'RitterSport': 120,
+            'Snickers': 70,
+            'Mars': 60,
+        }
+
+        if name in ['CocaCola', 'Sprite']:
+            return Liquid(name, price_dict.get(name))
+        elif name in ['Alenka', 'RitterSport', 'Snickers', 'Mars']:
+            return Snack(name, price_dict.get(name))
+        else:
+            raise NotImplemented
+
+
 class VendingMachine:
-    def __init__(self, items, cash):
+    def __init__(self):
         # which items are presented on which shelves
-        self._items = items
+        self._items = []
         # how much money inside machine to give change
-        self._cash = cash
+        self._cash = []
         # which purchases users made
         self._purchases_history = []
+
+    def add_item(self, item):
+        self._items.append(item)
+
+    def add_cash(self, cash):
+        self._cash = cash
 
     @property
     def items(self):
@@ -20,42 +66,14 @@ class VendingMachine:
     def __gt__(self, other):
         return self.get_cash_amount() > other.get_cash_amount()
 
-    def __sub__(self, other):
-        union_keys = set(self._items.keys()).union(other.items.keys())
-        diff = {}
-        for k in union_keys:
-            diff[k] = max(0, self._items.get(k, 0) - other.items.get(k, 0))
-        return diff
 
-    def __or__(self, other):
-        union_keys = set(self._items.keys()).union(other.items.keys())
-        union = {}
-        for k in union_keys:
-            union[k] = self._items.get(k, 0) + other.items.get(k, 0)
-        return union
-        return self._items
-
-    def __and__(self, other):
-        union_keys = set(self._items.keys()).union(other.items.keys())
-        intersection = {}
-        for k in union_keys:
-            intersection_key = min(self._items.get(k, 0), other.items.get(k, 0))
-            if intersection_key > 0:
-                intersection[k] = intersection_key
-        return intersection
-        return self._items
-
-items_a = {'Alenka': 2, 'RitterSport': 4, 'Snickers': 4}
+items_a = {'Alenka': 2, 'RitterSport': 1, 'Sprite': 1}
 cash_left_a = {10: 100, 5: 100, 1: 99}
-A = VendingMachine(items_a, cash_left_a)
+A = VendingMachine()
+for k, v in items_a.items():
+    for _ in range(v):
+        A.add_item(ProductFactory.create_product(k))
+A.add_cash(cash_left_a)
 
-items_b = {'Alenka': 3, 'RitterSport': 5, 'Snickers': 0, 'Mars': 0}
-cash_left_b = {10: 100, 5: 100, 1: 100}
-B = VendingMachine(items_b, cash_left_b)
 
 print(A)
-print(B)
-print(A < B)
-print(A - B)
-print(A & B)
-print(A | B)
